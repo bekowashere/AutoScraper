1f9a0842d548aabe267e3c1d3946e236f870dca3
Berke <uberke.karatas@gmail.com>
2023-06-20 22:16:53 +0300

    BrandScraper create_images method

--- scraper.py
+++ scraper.py
@@ -17,8 +17,8 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 class BrandScraper:
     def __init__(self):
         self.baseurl: str = "https://www.autoevolution.com/cars/"
-        self.folder_name: str = "all_brand_data.json"
         self.html_element = {'itemtype': 'https://schema.org/Brand'}
+        self.folder_name: str = "all_brand_data.json"
         self.brand_data: list = []
 
     def collect_data(self):
@@ -64,6 +64,26 @@ class BrandScraper:
 
         print(f"{self.folder_name} successfully created")
 
+    def create_images(self):
+        # open folder
+        with open(self.folder_name, 'r', encoding="UTF-8") as f:
+            data = json.load(f)
+
+        # inside brand_logo folder
+        os.chdir(os.path.join(os.getcwd(), 'brand_logo'))
+
+        for obj in data:
+            with open(obj["brand_image_path"], 'wb') as f:
+                b_im = requests.get(obj["brand_image_url"])
+                f.write(b_im.content)
+
+        print("Brand Images successfully created")
+
+        # exit folder
+        os.chdir('..')
+
 
 brand = BrandScraper()
 brand.collect_data()
+brand.create_json()
+brand.create_images()
